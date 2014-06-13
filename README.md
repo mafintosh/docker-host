58300b32c809a8c5563f53067ec86836cd80ad13
Mathias Buus <mathiasbuus@gmail.com>
2014-06-14 01:29:44 +0200

    returning parsed object

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # docker-host
 
-Tiny module that converts a host to docker (usually `$DOCKER_HOST`) to a standard http url
+Tiny module that parses docker remote host (usually `$DOCKER_HOST`)
 
 ```
 npm install docker-host
@@ -14,11 +14,11 @@ npm install docker-host
 var host = require('docker-host')
 
 var h = host()
-console.log(h) // will print something like http://localhost:2375
+console.log(h) // will print something like {socketPath:'/var/run/docker.sock'}
                // if $DOCKER_HOST is set that will be used as foundation as well
 
 var h = host('tcp://:2375')
-console.log(h) // will print http://localhost:2375
+console.log(h) // will print {host:'localhost', port:2375}
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = function(remote) {
-  if (!remote) remote = process.env.DOCKER_HOST || 'localhost:2375'
+  if (!remote) remote = process.env.DOCKER_HOST || 'unix:///var/run/docker.sock'
+  if (typeof remote === 'number') return {host:'localhost', port:remote}
+  if (remote.indexOf('://') === -1) remote = 'tcp://'+remote
 
-  if (typeof remote === 'number')   remote = 'localhost:'+remote
-  if (remote.indexOf('://') === -1) remote = 'http://'+remote
+  var parts = remote.match(/^(.+):\/\/([^:]*)(?::(\d+))?$/)
+  if (!parts) throw new Error('Invalid docker remote: '+remote)
 
-  return remote
-    .replace('://:', '://localhost:')
-    .replace('://0.0.0.0', '://localhost')
-    .replace(/^tcp:\/\//, 'http://')
+  var protocol = parts[1]
+  var host = parts[2] || 'localhost'
+  var port = parts[3] || 2375
+
+  if (protocol === 'unix' || host[0] === '/') return {socketPath:host}
+  if (host === '0.0.0.0') host = 'localhost'
+  return {host:host, port:parseInt(port, 10)}
 }
